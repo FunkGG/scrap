b207c7e2270e6ac8a410d5628fe774d79ac66785
FunkGG <huangjiyufg@163.com>
2019-03-21 20:01:04 +0800

    硬盘缓存与MongoDB缓存
    Signed-off-by: FunkGG <huangjiyufg@163.com>

---
+++ cache.py
@@ -0,0 +1,81 @@
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+import os
+import re
+import urllib.parse
+import pickle
+import zlib
+
+
+class DiskCache:
+    def __init__(self, cache_dir='cache', expires=timedelta(days=1), compress=True):
+        self.cache_dir = cache_dir
+        self.expires = expires
+        self.compress = compress
+
+    def url_to_path(self, url):
+        components = urllib.parse.urlsplit(url)
+        path = components.path
+        if not path:
+            path = '/index.html'
+        elif path.endswith('/'):
+            path += '/index.html'
+        filename = components.netloc + path + components.query
+        filename = re.sub('[^/0-9a-zA-Z\-.,;_ ]', '_', filename)
+        filename = '/'.join(segment[:255] for segment in filename.split('/'))
+        return os.path.join(self.cache_dir, filename)
+
+    def __getitem__(self, url):
+        path = self.url_to_path(url)
+        if os.path.exists(path):
+            with open(path, 'rb') as fp:
+                data = fp .read()
+                if self.compress:
+                    data = zlib.decompress(data)
+                result, timestamp = pickle.loads(data)
+                if self.has_expired(timestamp):
+                    raise KeyError(url + 'has expired')
+                return result
+        else:
+            raise KeyError(url + 'does not exist')
+
+    def __setitem__(self, url, result):
+        path = self.url_to_path(url)
+        folder = os.path.dirname(path)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        data = pickle.dumps((result, datetime.utcnow()))
+        if self.compress:
+            data = zlib.compress(data)
+        with open(path, 'wb') as fp:
+            fp.write(data)
+
+    def has_expired(self, timestamp):
+        return datetime.utcnow() > timestamp + self.expires
+
+
+class MongoCache:
+    def __init__(self, client=None, expire=timedelta(days=5)):
+        # 如果没传入client，连接默认的MongoClient
+        self.client = MongoClient('localhost', 27017) if client is None else client
+        # 创建collection(集合)储存缓存页面
+        self.db = client.cache
+        self.db.webpage.create_index('timestamp',
+                                     expireAfterSeconds=expire.total_seconds())
+
+    def __getitem__(self, url):
+        record = self.db.webpage.find_one({'_id':url})
+        if record:
+            return record['result']
+        else:
+            raise KeyError(url + 'does not exist')
+
+    def __setitem__(self, url, result):
+        record = {'result': result, 'timestamp': datetime.utcnow()}
+        self.db.webpage.update({'_id': url}, {'$set': record}, upsert=True)
+
+
+
+
+
--- crawl_function.py
+++ crawl_function.py
@@ -1,11 +1,12 @@
-from download_version_x import download_v1_2
-import urllib.parse  #解析url的模块
+import urllib.request
+import urllib.parse  # 解析url的模块
 import urllib.robotparser as robotparser
 import datetime
 import time
 import lxml.html
 import csv
 import re
+import random
 
 
 class Throttle:
@@ -35,6 +36,7 @@ def get_links(html):
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
     return webpage_regex.findall(html.decode('utf-8'))
 
+
 def normalize(seed_url, link):
     link, _ = urllib.parse.urldefrag(link)
     return urllib.parse.urljoin(seed_url, link)
@@ -61,30 +63,54 @@ class ScrapeCallback:
             self.writer.writerow(row)
 
 
-def download(url, headers, proxy=None, num_retries=1, data=None):
-    print('Downloading:', url)
-    request = urllib.request.Request(url, data, headers)
-    opener = urllib.request.build_opener()
-    if proxy:
-        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
-        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
-    try:
-        response = opener.open(request)
-        html = response.read()
-    except urllib.request.URLError as e:
-        print('Download error:',e.reason)
-        html = None
-        if hasattr(e, 'code'):
-            code = e.code
-            if num_retries > 0 and 500 <= code < 600:
-                return download(url, headers, proxy, num_retries - 1, data)
-    return html
+class Downloader:
+    def __init__(self, delay=5, user_agent='FunkGG', proxies=None, num_retries=1, cache=None):
+        self.throttle = Throttle(delay)
+        self.user_agent = user_agent
+        self.proxies = proxies
+        self.num_retries = num_retries
+        self.cache = cache
 
+    def __call__(self, url):
+        result = None
+        if self.cache:
+            try:
+                result = self.cache[url]
+            except KeyError:
+                pass
+            else:
+                if self.num_retries > 0 and 500 <= result['code'] < 600:
+                    result = None
+        if result is None:
+            self.throttle.wait(url)
+            proxy = random.choice(self.proxies) if self.proxies else None
+            headers = {'User-agent': self.user_agent}
+            result = self.download(url, headers, proxy, self.num_retries)
+            if self.cache:
+                self.cache[url] = result
+        return result['html']
 
-
-
-
-
+    def download(self, url, headers, proxy, num_retries, data=None):
+        print('Downloading:', url)
+        request = urllib.request.Request(url, data, headers)
+        opener = urllib.request.build_opener()
+        if proxy:
+            proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib.request.ProxyHandler(proxy_params))
+        try:
+            response = opener.open(request)
+            html = response.read()
+            code = response.code
+        except urllib.request.URLError as e:
+            print('Download error:', e.reason)
+            html = None
+            if hasattr(e, 'code'):
+                code = e.code
+                if num_retries > 0 and 500 <= code < 600:
+                    return self.download(url, headers, proxy, num_retries - 1, data)
+            else:
+                code = None
+        return {'html': html, 'code': code}
 
 
 
